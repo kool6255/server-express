b818750c1581b45985857da9745d37cd496d6422
Ivan <ivan.pap18@gmail.com>
2018-11-01 10:50:37 -0500

    subida de archivos

---
+++ server/routes/upload.js
@@ -0,0 +1,153 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const Usuario = require('../models/usuario');
+const Producto = require('../models/producto');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+app.use(fileUpload());
+
+app.put('/upload/:tipo/:id', (req, res) => {
+    let tipo = req.params.tipo;
+    let id = req.params.id;
+    if (!req.files) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'no se ha seleccionado ningun archivo'
+            }
+        });
+    }
+
+    let tiposValidos = ['productos', 'usuarios'];
+
+    if (tiposValidos.indexOf(tipo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'las tipos validas son ' + tiposValidos.join(', '),
+            }
+        })
+    }
+
+    let archivo = req.files.archivo;
+    let nombreCortado = archivo.name.split('.');
+    let extension = nombreCortado[nombreCortado.length - 1];
+
+
+    let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
+
+    if (extensionesValidas.indexOf(extension) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'las extensiones validas son ' + extensionesValidas.join(', '),
+                ext: extension
+            }
+        })
+    }
+
+    let nombreArchivo = `${ id }-${ new Date().getMilliseconds() }.${ extension }`;
+
+    archivo.mv(`uploads/${tipo}/${ nombreArchivo }`, (err) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        };
+        if (tipo === 'usuarios') {
+            imagenUsuario(id, res, nombreArchivo);
+        } else {
+            imagenProducto(id, res, nombreArchivo);
+        }
+
+    });
+});
+
+function imagenUsuario(id, res, nombreArchivo) {
+    Usuario.findById(id, (err, usuarioDB) => {
+        if (err) {
+            borrarArchivo(nombreArchivo, 'usuarios');
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!usuarioDB) {
+            borrarArchivo(nombreArchivo, 'usuarios');
+            return res.status(500).json({
+                ok: false,
+                err: {
+                    message: 'usuario no existe'
+                }
+            });
+        }
+
+        borrarArchivo(usuarioDB.img, 'usuarios');
+
+        usuarioDB.img = nombreArchivo;
+        usuarioDB.save((err, usuarioGuardado) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                usuario: usuarioGuardado,
+                img: nombreArchivo
+            })
+        });
+
+    });
+}
+
+function imagenProducto(id, res, nombreArchivo) {
+    Producto.findById(id, (err, productoDB) => {
+        if (err) {
+            borrarArchivo(nombreArchivo, 'productos');
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!productoDB) {
+            borrarArchivo(nombreArchivo, 'productos');
+            return res.status(500).json({
+                ok: false,
+                err: {
+                    message: 'producto no existe'
+                }
+            });
+        }
+
+        borrarArchivo(productoDB.img, 'productos');
+
+        productoDB.img = nombreArchivo;
+        productoDB.save((err, productoGuardado) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                producto: productoGuardado,
+                img: nombreArchivo
+            })
+        });
+
+    });
+}
+
+function borrarArchivo(nombreImagen, tipo) {
+    let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${nombreImagen}`);
+    if (fs.existsSync(pathImagen)) {
+        fs.unlinkSync(pathImagen);
+    }
+}
+
+module.exports = app;
